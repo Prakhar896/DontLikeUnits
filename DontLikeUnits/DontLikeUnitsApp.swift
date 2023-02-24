f466fd47d18b53d750f2b512b350273f179765c5
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2023-02-24 19:14:15 +0800

    Initial Commit

---
+++ DontLikeUnits/DontLikeUnitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DontLikeUnitsApp.swift
+//  DontLikeUnits
+//
+//  Created by Prakhar Trivedi on 24/2/23.
+//
+
+import SwiftUI
+
+@main
+struct DontLikeUnitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
